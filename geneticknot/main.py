c51e6d844209ccc7512aeb2346de156ccf8af21a
MarekGn <marek.gna@gmail.com>
2020-01-07 00:45:15 +0100

    delete main function

--- geneticknot/main.py
+++ geneticknot/main.py
@@ -1,5 +1,4 @@
 from geneticknot.Population import Population
-from geneticknot.play_utils import play_with_ai
 from tqdm import tqdm
 from geneticknot.s3_utils import *
 
@@ -37,29 +36,3 @@ def distributed_training(pop_size, input_size, output_size, hidden_layers, board
         pop.cal_probability(alpha=alpha)
         pop.get_new_pop()
         pop.mutate_population(mutation_probability=mutation_probability)
-
-
-if __name__ == "__main__":
-    iterations = 10
-    hidden_layers = [14, 14]
-    input_size = 9
-    output_size = 9
-    pop_size = 50
-    board_shape = (3, 3)
-    mutation_probability = 0.02
-    alpha = 1.3
-    workers_num = 1
-    bucket_name = 'besttictactoe'
-
-    # distributed_training(
-    #     pop_size=pop_size,
-    #     input_size=input_size,
-    #     output_size=output_size,
-    #     hidden_layers=hidden_layers,
-    #     board_shape=board_shape,
-    #     workers_num=workers_num,
-    #     alpha=alpha,
-    #     mutation_probability=mutation_probability,
-    #     iterations=iterations,
-    #     bucket_name=bucket_name
-    # )
